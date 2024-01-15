1259dc92da08cab9a26fee5c87808b49ebc026b1
gabriel <gabriel@techdog.com.br>
2024-01-15 16:46:42 -0300

    introduces Command enum & Buf types

--- src/kv.rs
+++ src/kv.rs
@@ -1,18 +1,20 @@
-use std::{collections::HashMap, path::Path};
+use serde::{Serialize, Deserialize};
+
 use crate::{KvsError, Result};
+use std::{collections::HashMap, path::Path, ops::Range, io::{Read, Seek, BufReader, SeekFrom, Write, BufWriter}};
 
 pub struct KvStore {
-    store: HashMap<String, String>
+    store: HashMap<String, String>,
 }
 
 impl KvStore {
-    pub fn open(path: &Path) -> Result<Self>{
+    pub fn open(path: &Path) -> Result<Self> {
         todo!()
     }
     pub fn new() -> Self {
-        Self { 
-            store: HashMap::new()
-         }
+        Self {
+            store: HashMap::new(),
+        }
     }
 
     pub fn get(&self, key: String) -> Result<Option<String>> {
@@ -24,7 +26,7 @@ impl KvStore {
         Ok(())
     }
 
-    pub fn remove(&mut self, key: String) -> Result<()>{
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.store.remove(&key);
         Ok(())
     }
@@ -33,5 +35,96 @@ impl KvStore {
 impl Default for KvStore {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Set { key: String, value: String },
+    Remove { key: String },
+}
+
+impl Command {
+    fn set(key: String, value: String) -> Self {
+        Self::Set { key, value }
+    }
+
+    fn remove(key: String) -> Command {
+        Self::Remove { key }
+    }
+}
+
+struct CommandPos {
+    gen: u64,
+    pos: u64,
+    len: u64,
+}
+
+impl From<(u64, Range<u64>)> for CommandPos {
+    fn from((gen, range): (u64, Range<u64>)) -> Self {
+        CommandPos { gen, pos: range.start, len: range.end - range.start }
+    }
+}
+
+struct BufReaderWithPos<R: Read + Seek> {
+    reader: BufReader<R>,
+    pos: u64
+}
+
+impl<R: Read + Seek> BufReaderWithPos<R>  {
+    fn new(mut inner: R) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(Self { reader: BufReader::new(inner), pos })
+    }
+    
+}
+
+impl<R: Read + Seek> Read for BufReaderWithPos<R>  {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let len = self.reader.read(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+}
+
+impl<R: Read + Seek> Seek for BufReaderWithPos<R>  {
+    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
+        self.pos = self.reader.seek(pos)?;
+        Ok(self.pos)
+    }
+}
+
+struct BufWriterWithPos<W: Write + Seek> {
+    writer: BufWriter<W>,
+    pos: u64
+}
+
+impl <W: Write + Seek> BufWriterWithPos<W> {
+    fn new(mut inner: W) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(BufWriterWithPos {
+            writer: BufWriter::new(inner),
+            pos,
+        })
+    }
+}
+
+impl <W: Write + Seek> Write for BufWriterWithPos<W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let len = self.writer.write(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+
+impl <W: Write + Seek> Seek for BufWriterWithPos<W>  {
+    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
+        self.pos = self.writer.seek(pos)?;
+        Ok(self.pos)
     }
 }
